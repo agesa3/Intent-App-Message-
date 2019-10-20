57a42e7fa5038ec7da81720ea24c010c29720df0
AGESA <collinsagesa2000@gmail.com>
2019-10-20 16:24:41 +0300

    Intent App

---
+++ app/src/main/java/com/example/intentapp/IntentMessage.java
@@ -0,0 +1,34 @@
+package com.example.intentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class IntentMessage extends AppCompatActivity {
+    public static final String EXTRA_REPLY = "com.example.intentapp.extra.REPLY";
+    private EditText mReply;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+        mReply = findViewById(R.id.editText_second);
+
+    }
+
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK,replyIntent);
+        finish();
+    }
+}
